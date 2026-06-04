f2794a8e992eea58199dec079c059a31c4c9edef
Ruben Vitt <dev@rubeen.dev>
2026-06-04 15:44:27 +0200

    feat(admin): Aufgabenkatalog über Loader + useSuspenseQuery/useMutation

--- src/routes/admin.katalog.tsx
+++ src/routes/admin.katalog.tsx
@@ -1,6 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { CatalogPage } from '../admin/CatalogPage';
+import { AdminFehler } from '../admin/AdminFehler';
+import { adminTasksQuery } from '../admin/queries';
 
 export const Route = createFileRoute('/admin/katalog')({
+  loader: ({ context }) => context.queryClient.ensureQueryData(adminTasksQuery),
   component: CatalogPage,
+  errorComponent: AdminFehler,
 });
