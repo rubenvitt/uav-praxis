6edce7cd32f5d76a16047d87578bb11703193cfd
Ruben Vitt <dev@rubeen.dev>
2026-06-04 15:35:53 +0200

    feat(admin): Teilnehmer-Detail über Loader + useSuspenseQuery/useMutation

--- src/routes/admin.participants.$participantId.tsx
+++ src/routes/admin.participants.$participantId.tsx
@@ -1,8 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { ParticipantDetailPage } from '../admin/ParticipantDetailPage';
+import { AdminFehler } from '../admin/AdminFehler';
+import { participantDetailQuery } from '../admin/queries';
 
 export const Route = createFileRoute('/admin/participants/$participantId')({
+  loader: ({ context, params }) =>
+    context.queryClient.ensureQueryData(participantDetailQuery(params.participantId)),
   component: ParticipantDetailRoute,
+  errorComponent: AdminFehler,
 });
 
 function ParticipantDetailRoute() {
